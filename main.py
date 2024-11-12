6ab666b53ab230c1ff4c9c27ff70d004483bc1ea
Gleb <glebpetrof2007@gmail.com>
2024-11-12 12:07:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-a =str(input("Введите cтроку "))
-gl=('у','е','ы','а','о','э','я','и','ю','У','Е','Ы','А','О','Э','Я','И','Ю')
-gl_count=0
+a =str(input("Введите cтроку ")) #Ввод строки
+gl=('у','е','ы','а','о','э','я','и','ю','У','Е','Ы','А','О','Э','Я','И','Ю') #список гласных
+gl_count=0 #инициализация переменных
 space_count=0
-for i in range(len(a)):
+for i in range(len(a)): #перебор строки
     for b in range(len(gl)):
         if(gl[b]==a[i]):
             gl_count+=1
@@ -12,8 +12,8 @@ for i in range(len(a)):
           
 
     
-sogl_count=len(a)-gl_count-space_count
-print(f"длина строки {len(a)}")
+sogl_count=len(a)-gl_count-space_count #подсчет числа согласных 
+print(f"длина строки {len(a)}") #вывод
 print(f"количество гласных {gl_count}")
 print(f"количество согласных {sogl_count}")
 
